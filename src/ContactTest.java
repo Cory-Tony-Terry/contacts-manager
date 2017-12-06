dddb18da8bf8c026d07729d3c2832e649f6d815a
terryhale <aggiefan17@yahoo.com>
2017-12-06 11:07:06 -0600

    added scanner to main

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -1,6 +1,22 @@
+import util.Input;
+
 public class ContactTest {
 
     public static void main(String[] args) {
+
+        Input input = new Input();
+
+        System.out.println("What would you like to do?\n");
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+
+
+
 
     ContactFileIO test = new ContactFileIO ();
         System.out.println (test.getContacts ());
