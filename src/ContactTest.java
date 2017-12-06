b918dde6729ac44d27838b84607ef73aa5b8af31
Cory Imel <cory.imel@gmail.com>
2017-12-06 17:52:45 -0600

    moved methods below main

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -6,50 +6,8 @@ import java.text.ParseException;
 import java.util.HashMap;
 
 public class ContactTest {
-
-        private static ContactFileIO contactFileIO = new ContactFileIO ();
-        private static HashMap<String, Contact> contactHashMap = new HashMap<>(contactFileIO.getContacts());
-
-    public static void outputFormat(String name) throws ParseException {
-
-
-
-            try {
-                String phoneMaskSevenDigit = "###-####";
-                String phoneMaskNineDigit = "###-###-####";
-
-                MaskFormatter maskFormatter= new MaskFormatter(phoneMaskNineDigit);
-                maskFormatter.setValidCharacters("0123456789");
-                maskFormatter.setValueContainsLiteralCharacters(false);
-
-                if (contactHashMap.containsKey(name)) {
-                    String fullName = contactHashMap.get(name).getFirstName() + " " + contactHashMap.get(name).getLastName();
-                    String phoneNumber = contactHashMap.get(name).getPhoneNumber();
-                    String formattedPhoneNumber = maskFormatter.valueToString(phoneNumber);
-
-                    System.out.printf("%-15s | %-12s |", fullName, formattedPhoneNumber);
-                    System.out.println ();
-                } else {
-                    System.out.println("name does not exist");
-                }
-            } catch(java.text.ParseException e) {
-                System.out.println(e.getMessage());
-            }
-
-
-    }
-
-    public static void addNameToArray(String firstName, String lastName, String phoneNumber) throws ParseException {
-        String key = firstName.toLowerCase() + " " + lastName.toLowerCase();
-        contactHashMap.putIfAbsent (key,  new Contact(firstName, lastName, phoneNumber));
-        outputFormat (key);
-
-    }
-    public static void removeNamefromArray(String key) {
-        contactHashMap.remove (key);
-    }
-
-
+    private static ContactFileIO contactFileIO = new ContactFileIO ();
+    private static HashMap<String, Contact> contactHashMap = new HashMap<>(contactFileIO.getContacts());
 
     public static void main(String[] args) throws ParseException {
         boolean begin = true;
@@ -121,6 +79,48 @@ public class ContactTest {
         contactFileIO.setContacts (contactHashMap);
 
     }
+
+    public static void outputFormat(String name) throws ParseException {
+
+
+
+        try {
+            String phoneMaskSevenDigit = "###-####";
+            String phoneMaskNineDigit = "###-###-####";
+
+            MaskFormatter maskFormatter= new MaskFormatter(phoneMaskNineDigit);
+            maskFormatter.setValidCharacters("0123456789");
+            maskFormatter.setValueContainsLiteralCharacters(false);
+
+            if (contactHashMap.containsKey(name)) {
+                String fullName = contactHashMap.get(name).getFirstName() + " " + contactHashMap.get(name).getLastName();
+                String phoneNumber = contactHashMap.get(name).getPhoneNumber();
+                String formattedPhoneNumber = maskFormatter.valueToString(phoneNumber);
+
+                System.out.printf("%-15s | %-12s |", fullName, formattedPhoneNumber);
+                System.out.println ();
+            } else {
+                System.out.println("name does not exist");
+            }
+        } catch(java.text.ParseException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void addNameToArray(String firstName, String lastName, String phoneNumber) throws ParseException {
+        String key = firstName.toLowerCase() + " " + lastName.toLowerCase();
+        contactHashMap.putIfAbsent (key,  new Contact(firstName, lastName, phoneNumber));
+        outputFormat (key);
+
+    }
+    public static void removeNamefromArray(String key) {
+        contactHashMap.remove (key);
+    }
+
+
+
 
 
 }
