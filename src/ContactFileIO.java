c2c415d9cbe8f26e8621bdb7d0e5dd6922630294
terryhale <aggiefan17@yahoo.com>
2017-12-06 10:02:01 -0600

    added write file and setContacts method

--- src/ContactFileIO.java
+++ src/ContactFileIO.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContactFileIO {
     private HashMap<String, Contact> contacts = new HashMap <> ();
@@ -13,21 +14,22 @@ public class ContactFileIO {
     private Path dataFile = Paths.get(directory, filename);
 
     public ContactFileIO() {
-        readFile ();
+        readFile();
     }
 
     public HashMap <String, Contact> getContacts() {
+
         return this.contacts;
     }
 
-    private  void readFile (){
+    private  void readFile() {
         try {
 
             List<String> currentContacts = Files.readAllLines(dataFile);
 
             for(String contact : currentContacts) {
                 List<String> contactArr = Arrays.asList(contact.split(","));
-
+                System.out.println(contactArr);
                 String key = contactArr.get(0) + " " + contactArr.get(1);
                 this.contacts.put(key, new Contact(contactArr.get(0), contactArr.get(1), contactArr.get(2)));
 
@@ -38,4 +40,16 @@ public class ContactFileIO {
          }
      }
 
+     private void writeFile() {
+         for (Map.Entry<String, Contact> entry : this.contacts.entrySet()) {
+//             properties.put(entry.getKey(), entry.getValue());
+             System.out.println(entry.getKey());
+         }
+
+     }
+
+    public void setContacts(HashMap<String, Contact> contacts) {
+        this.contacts = contacts;
+        writeFile();
+    }
 }
--- src/ContactTest.java
+++ src/ContactTest.java
@@ -1,8 +1,12 @@
 public class ContactTest {
+
     public static void main(String[] args) {
 
     ContactFileIO test = new ContactFileIO ();
         System.out.println (test.getContacts ());
+
+        test.setContacts(test.getContacts());
+
     }
 
 }
