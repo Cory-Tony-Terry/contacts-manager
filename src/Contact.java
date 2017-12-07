52421d98f934de0bc6d1d7c31ffb03b1708fd23d
Cory Imel <cory.imel@gmail.com>
2017-12-06 18:36:41 -0600

    cleaned unused code

--- src/Contact.java
+++ src/Contact.java
@@ -4,7 +4,7 @@ public class Contact {
     private String lastName;
     private String phoneNumber;
 
-    public Contact(String firstName, String lastName, String phoneNumber) {
+    private Contact(String firstName, String lastName, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
